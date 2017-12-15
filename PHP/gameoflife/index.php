1c92d725663fe56ef9bab9fad79ebeae111e6bcd
vanjavk <vanja.buturac@gmail.com>
2017-12-15 14:15:05 +0100

    Code is nicer now

--- PHP/gameoflife/index.php
+++ PHP/gameoflife/index.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
-
+<!--Good starting position: vanjavk.me/gameoflife/?n=10&m=8&aliens=3,3;2,3;3,2;3,1;1,2 -->
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="/css/default.css">
 		<meta charset="UTF-8">
-		<title>Title of the document</title>
+		<title>Game of life</title>
 	</head>
 	<body>
 		
-
-
 <?php
-
 function printGrid($n,$m,$aliens){
 	for($j=0; $j<$m; $j++)
 	{
@@ -51,32 +48,29 @@ function monthGrid($n,$m,$aliens){
 					
 				}
 			}
-
-			#foreach($coords as $z){
-			#	echo $z[0].' '.$z[1].'<br>';
-			#}
-			#echo "<br>";
 			$susjedi=0;
-			foreach($coords as $coord){
-				if (in_array($coord,$aliens)){
+			foreach($coords as $coord)
+			{
+				if (in_array($coord,$aliens))
+				{
 					$susjedi++;
 				}
 			}
-			if (in_array(array($i,$j),$aliens)){
-				if ($susjedi==2 or $susjedi==3){
+			if (in_array(array($i,$j),$aliens))
+			{
+				if ($susjedi==2 or $susjedi==3)
+				{
 					$aliens1[]=array($i,$j);
 				}
 			}
 			else
 			{
-				if ($susjedi==3){
+				if ($susjedi==3)
+				{
 					$aliens1[]=array($i,$j);
 				}
-			}
-			#echo "<br>";
-			
+			}	
 		}
-		
 	}
 	return $aliens1;
 }
@@ -86,24 +80,28 @@ function parseAliens($aliens){
 
 $n=intval($_GET["n"]);
 $m=intval($_GET["m"]);
-
 $aliens=parseAliens($_GET["aliens"]);
 $aliens1=parseAliens($_GET["aliens"]);
+
 $aliens=array_map(function($i) {return explode(',',$i);},explode(';',$aliens));
-echo "Game of life!";
-echo '<br>';
-#$aliens1 = array(array(3,3),array(2,3),array(3,2),array(3,1),array(1,2));
+echo "Game of life - by vanjavk!";
+echo '<br><br>';
+
 $aliens=monthGrid($n,$m,$aliens);
 printGrid($n,$m,$aliens);
+
 $aliensstr="";
 foreach($aliens as $a){
 	$aliensstr.=$a[0].','.$a[1].";";
 }
 $aliensstr=substr($aliensstr, 0, -1);
-#$_GET["aliens"]=$aliensstr;
-#echo $aliensstr;
 $url = "http://".$_SERVER['HTTP_HOST'].str_replace($aliens1,$aliensstr,parseAliens($_SERVER['REQUEST_URI']));
-echo '<a href="'.$url.'">Fast forward 1 month</a>';
+echo '<br><a href="'.$url.'">Next step</a>';
+echo '<br>';
+echo '<br>';
+$source_code='https://github.com/vanjavk/Programming/blob/master/PHP/gameoflife';
+echo '<a href="'.$source_code.'">Source code</a>';
+
 
 
 ?>
